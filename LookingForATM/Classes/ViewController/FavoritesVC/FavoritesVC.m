2ae8c6af495dcca130daff8198ba1326d6cc876f
Le Kim Tuan <kimtuan257@gmail.com>
2015-10-07 14:45:44 +0700

    Done!!!

--- LookingForATM/Classes/ViewController/FavoritesVC/FavoritesVC.m
+++ LookingForATM/Classes/ViewController/FavoritesVC/FavoritesVC.m
@@ -15,24 +15,33 @@
 #import "ATMHistoryModel.h"
 #import "AppDelegate.h"
 #import "DetailVC.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface FavoritesVC ()<UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate> {
+@interface FavoritesVC ()<UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate, CLLocationManagerDelegate> {
     AppDelegate *_myAppDelegate;
+    CLLocationManager *_locationManager;
 }
 @property (weak, nonatomic) IBOutlet UITableView *favoritesTableView;
 @property (weak, nonatomic) IBOutlet UITableView *historyTableView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @end
 
 @implementation FavoritesVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [_titleLabel setText:@"ATM Searched"];
     [_favoritesTableView setHidden:YES];
-    [_historyTableView setHidden:YES];
     _myAppDelegate = (AppDelegate*)[[UIApplication sharedApplication]delegate];
     [_favoritesTableView registerNib:[UINib nibWithNibName:@"HomeCell" bundle:nil] forCellReuseIdentifier:@"HomeCell"];
     [_historyTableView registerNib:[UINib nibWithNibName:@"HomeCell" bundle:nil] forCellReuseIdentifier:@"HomeCell"];
+    
+    _locationManager = [CLLocationManager new];
+    _locationManager.delegate = self;
+    _locationManager.distanceFilter = 10;//10 met
+    _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;//do chinh xac gan 10 met
+    [_locationManager startUpdatingLocation];
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
@@ -41,9 +50,11 @@
 
 - (IBAction)segmentAction:(id)sender {
     if (_segmentedControl.selectedSegmentIndex == 0) {
+        [_titleLabel setText:@"ATM Searched"];
         [_historyTableView setHidden:NO];
         [_favoritesTableView setHidden:YES];
     }else{
+        [_titleLabel setText:@"ATM Favorites"];
         [_historyTableView setHidden:YES];
         [_favoritesTableView setHidden:NO];
     }
@@ -75,6 +86,12 @@
     [_historyTableView reloadData];
 }
 
+#pragma mark - Location Manager Delegate
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    [_historyTableView reloadData];
+    [_favoritesTableView reloadData];
+}
+
 #pragma mark - TableView DataSource and Delegate
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if (tableView == self.favoritesTableView) {
@@ -86,7 +103,7 @@
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     HomeCell *cell = [tableView dequeueReusableCellWithIdentifier:@"HomeCell" forIndexPath:indexPath];
-    if (tableView == self.favoritesTableView) {
+    if (tableView == _favoritesTableView) {
         ATMFavorites *favorites = self.fetchFavorite.fetchedObjects[indexPath.row];
         cell.nameLabel.text = favorites.name;
         cell.addressLabel.text = favorites.address;
@@ -112,22 +129,22 @@
 }
 
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (tableView == self.favoritesTableView) {
+    if (tableView == _favoritesTableView) {
         ATMFavorites *favorites = self.fetchFavorite.fetchedObjects[indexPath.row];
         [favorites MR_deleteEntity];
         [[NSManagedObjectContext MR_defaultContext]MR_saveToPersistentStoreAndWait];
-        [self.favoritesTableView reloadData];
+        [_favoritesTableView reloadData];
     }else{
         ATMHistory *history = self.fetchHistory.fetchedObjects[indexPath.row];
         [history MR_deleteEntity];
         [[NSManagedObjectContext MR_defaultContext]MR_saveToPersistentStoreAndWait];
-        [self.historyTableView reloadData];
+        [_historyTableView reloadData];
     }
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (tableView == self.favoritesTableView) {
-        [self.favoritesTableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (tableView == _favoritesTableView) {
+        [_favoritesTableView deselectRowAtIndexPath:indexPath animated:YES];
         DetailVC *vc = [DetailVC new];
         ATMFavorites *favorite = self.fetchFavorite.fetchedObjects[indexPath.row];
         [self.navigationController pushViewController:vc animated:YES];
@@ -136,7 +153,7 @@
         vc.latitude = [favorite.latitude doubleValue];
         vc.longitude = [favorite.longitude doubleValue];
     }else{
-        [self.historyTableView deselectRowAtIndexPath:indexPath animated:YES];
+        [_historyTableView deselectRowAtIndexPath:indexPath animated:YES];
         DetailVC *vc = [DetailVC new];
         ATMHistory *history = self.fetchHistory.fetchedObjects[indexPath.row];
         [self.navigationController pushViewController:vc animated:YES];
